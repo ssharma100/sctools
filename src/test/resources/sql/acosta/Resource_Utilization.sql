9f1fda9008f272dc56b5f5dbbfd9b15c1ddcd6ac
Samir Sharma <samir_sharma@att.net>
2017-05-22 17:47:47 -0700

    Updated resrouce utilization report for reset

--- src/test/resources/sql/acosta/Resource_Utilization.sql
+++ src/test/resources/sql/acosta/Resource_Utilization.sql
@@ -75,12 +75,14 @@ ceil((sum(ExecutionDuration)/60)/5) as Hours_Per_Day,
 ceil((sum(ExecutionDuration)/60)) as Hours_Per_Week
 from continuity_activity_may18_wm where startdate >= '2018-05-21' and startdate <= '2018-05-25' group by ReqResource order by ReqResource;
 
+drop view resource_utilization_week5_wm;
 CREATE VIEW resource_utilization_week5_wm
 AS
 select ReqResource, sum(PlannedDuration) as Total_PlannedDuration, sum(ExecutionDuration) as Total_ExecutionDuration, 
 ceil((sum(ExecutionDuration)/60)/5) as Hours_Per_Day,
 ceil((sum(ExecutionDuration)/60)) as Hours_Per_Week
-from  continuity_activity_may18_wm where startdate >= '2018-05-28' and startdate <= '2018-06-01' group by ReqResource order by ReqResource;
+from  continuity_activity_may18_wm 
+where startdate >= '2018-05-28' and startdate <= '2018-06-01' group by ReqResource order by ReqResource;
 
 
 select * from resource_utilization_week1_wm;
@@ -92,10 +94,33 @@ select * from resource_utilization_week5_wm;
 --
 -- Utilization For Weekends
 --
-select COMPLETED_BY_EMPLOYEE_NO, '8am-5pm' as CONTY_SAT_SHIFT, dayname(CALL_STARTED_LOCAL) from continuity_actual_call_details where DAYNAME(CALL_STARTED_LOCAL) = 'Saturday';
+Create View continuity_assocaites_avail_sat
+as
+select distinct COMPLETED_BY_EMPLOYEE_NO as EMPLOYEE_NO, WEEK(CALL_STARTED_LOCAL, 0) as WEEK, date(CALL_STARTED_LOCAL) AS 'Start_Date', '8am-5pm' as CONTY_SAT_SHIFT, dayname(CALL_STARTED_LOCAL) from continuity_actual_call_details where DAYNAME(CALL_STARTED_LOCAL) = 'Saturday';
 
-CREATE TABLE 
-continuity_assciates_weekend 
-`EMPLOYEE_NO` int(9) PRIMARY,
+Create View continuity_assocaites_avail_sun
+as
+select distinct COMPLETED_BY_EMPLOYEE_NO as EMPLOYEE_NO, WEEK(CALL_STARTED_LOCAL, 0) as WEEK, date(CALL_STARTED_LOCAL) AS 'Start_Date', '8am-5pm' as CONTY_SUN_SHIFT, dayname(CALL_STARTED_LOCAL) from continuity_actual_call_details where DAYNAME(CALL_STARTED_LOCAL) = 'Sunday';
 
+select * from continuity_assocaites_avail_sat;
+select * from continuity_assocaites_avail_sun  order by week;
 
+drop view continuity_assocaites_avail_sat;
+drop view continuity_assocaites_avail_sun;
+
+--
+-- Create Associate View For Specific "Full Schedule"
+SELECT wk1.ReqResource, wk1.HOURS_PER_WEEK, CSAT.CONTY_SAT_SHIFT, CSUN.CONTY_SUN_SHIFT
+from resource_utilization_week1 as wk1
+LEFT OUTER JOIN continuity_assocaites_avail_sat AS CSAT on CSAT.EMPLOYEE_NO = wk1.reqresource and CSAT.week = 1
+LEFT OUTER JOIN continuity_assocaites_avail_sun AS CSUN on CSUN.EMPLOYEE_NO = wk1.ReqResource and CSUN.week = 6;
+
+select count(*) from resource_utilization_week1;
+
+select * from resource_utilization_week1 where ReqResource = '992325652';
+select * from continuity_assocaites_avail_sat where employee_no = '992325652';
+select * from continuity_assocaites_avail_sun where employee_no = '992325652';
+
+select * from resource_utilization_week1 where ReqResource = '992293148';
+select * from continuity_assocaites_avail_sat where employee_no = '992293148';
+select * from continuity_assocaites_avail_sun where employee_no = '992293148';
